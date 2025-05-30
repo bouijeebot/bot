c79e0e4c6dbdc9b0db4e03467928a1fb270672b9
bouijeebot <hejangelicahaglund@gmail.com>
2025-05-31 01:46:36 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,19 @@
 from flask import Flask, request
+import threading
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    return "Bouijee Bot is alive", 200
+@app.route("/", methods=["GET", "HEAD"])
+def index():
+    return "Bouijee Bot is alive!", 200
+
+@app.route("/", methods=["POST"])
+def webhook():
+    return "Webhook received!", 200
 
 def keep_alive():
-    app.run(host="0.0.0.0", port=8080)
+    def run():
+        app.run(host="0.0.0.0", port=8080)
+
+    t = threading.Thread(target=run)
+    t.start()
