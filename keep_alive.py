1b7a650908cb1b4ffc041475a80463d9d645c294
bouijeebot <hejangelicahaglund@gmail.com>
2025-05-31 02:02:33 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,18 +3,19 @@ import telebot
 import os
 
 app = Flask(__name__)
-bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(TOKEN)
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def home():
-    return "Bouijee Bot is running!"
+    return "Bouijee Bot is live! 💅", 200
 
-@app.route("/", methods=["POST"])
-def receive_update():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
+@app.route('/', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
     return '', 200
 
 def keep_alive():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
