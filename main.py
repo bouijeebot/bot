c0a2b9c2a077382ff350a7ab0fcff4f7c381e93c
bouijeebot <hejangelicahaglund@gmail.com>
2025-05-31 18:54:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,11 @@ import json
 def get_credentials():
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
     service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    
+    # FIXA PRIVATE KEY MED RIKTIGA RADBRYTNINGAR
+    if "private_key" in service_account_info:
+        service_account_info["private_key"] = service_account_info["private_key"].replace("\\n", "\n")
+
     return Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
 
 def log_signal_to_sheet(sheet_name, values):
