60d6ad6a645e5a9658a9d76965b87f11957538e9
bouijeebot <hejangelicahaglund@gmail.com>
2025-06-13 21:25:44 +0200

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,10 +1,10 @@
 import os
 import telebot
 
-TOKEN = os.getenv("TELEGRAM_TOKEN") or "DIN_TOKEN_HÄR"
+TOKEN = os.getenv("TELEGRAM_TOKEN") or "7692679752:AAH8QUrMAjnUBrnoy4pe0mMuTcosCRxfV2Q"
 bot = telebot.TeleBot(TOKEN)
 
-WEBHOOK_URL = "https://din-app.onrender.com/"  # <-- din Render-URL med /
+WEBHOOK_URL = "https://bot-ihfu.onrender.com"  # <-- din Render-URL med /
 bot.remove_webhook()
 bot.set_webhook(url=WEBHOOK_URL)
 print("✅ Webhook satt:", WEBHOOK_URL)
